3a03b74d221efcd37f531b09a1c67ca786f3a2d9
Benjamin Steinig <bsteinig23@gmail.com>
2021-07-27 13:31:05 -0400

    Reworked Nav

--- js/main.js
+++ js/main.js
@@ -3,8 +3,24 @@ function spinnyTime(){
     hamburger.classList.toggle("is-active");
 }
 
+function topMenu(){
+    if(document.querySelector('.menu-shown'))
+        document.querySelector('.menu-shown').scroll({top:0,behavior:'smooth'});
+}
+
+function showSidebar(){
+    var sidebar = document.querySelector(".right-sidebar");
+    sidebar.classList.toggle("sidebar-shown")
+}
+
 function showMenu(){
     spinnyTime();
     var menu = document.querySelector('.menu-popout');
     menu.classList.toggle('menu-shown')
+    topMenu();
+}
+
+function showLoader(){
+    var loader = document.querySelector('.loader');
+    loader.classList.toggle('loader-shown')
 }
